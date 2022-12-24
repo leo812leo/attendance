07c0979809350bf8c13b18a8802e40123ad4b4d7
leo812leo <leo_812leo@hotmail.com.tw>
2022-12-24 10:38:51 +0800

    feat: add user modules v3

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const app = express()
-const routes = require('./routes')
+const { engine } = require('express-handlebars')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3000
+const app = express()
 
 
+app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 app.use(routes)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -16,7 +21,8 @@ app.use(session({
   saveUninitialized: true
 }))
 
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+
+// 設定 port
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
