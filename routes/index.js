710cf3ea15b0627df49359330291aa20c7231acb
leo812leo <leo_812leo@hotmail.com.tw>
2022-12-24 11:09:50 +0800

    feat: admin models

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const users = require('./modules/users')
+const admin = require('./modules/admin')
 
 //路由
 router.use('/users', users)
+router.use('/admin', admin)
 
 module.exports = router
