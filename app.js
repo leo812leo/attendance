15e1566a3c20f6af46d760640993c95a0663ff61
leo812leo <leo_812leo@hotmail.com.tw>
2022-12-24 10:20:06 +0800

    feat: add user modules

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
 const app = express()
+const routes = require('./routes')
+const session = require('express-session')
 
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+
+app.use(routes)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // 設定 port 3000
 app.listen(3000, () => {
